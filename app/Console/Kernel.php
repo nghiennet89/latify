2ed584d77e1480fef8ea2f19e7312fbe7e6a3ca9
nghiennet89 <nghiennet89@gmail.com>
2022-05-27 15:09:59 +0700

    support run queue by laravel schedule (cronjob)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,15 +19,18 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     *
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('database:backup')->dailyAt('00:00');
         $schedule->command('passport:purge')->hourly();
-        $schedule->command('queue:restart')->everyTenMinutes();
-        $schedule->command('queue:work --daemon')->everyMinute()->withoutOverlapping()->appendOutputTo(storage_path('log/queue.log'));
+        if(env('RUN_QUEUE_VIA_COMMAND', false)) {
+            $schedule->command('queue:restart')->everyTenMinutes();
+            $schedule->command('queue:work --daemon')->everyMinute()->withoutOverlapping()->appendOutputTo(storage_path('log/queue-' . date('Y-m-d') . '.log'));
+        }
     }
 
     /**
@@ -37,7 +40,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
